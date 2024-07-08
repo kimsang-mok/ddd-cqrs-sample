08917b533560d8fd003dd434c4a2628454f19347
kimsang <kimsang.mok01@gmail.com>
2024-07-08 16:52:44 +0700

    add: create base sql repository

--- src/libs/ddd/index.ts
+++ src/libs/ddd/index.ts
@@ -2,4 +2,4 @@ export * from './repository.port';
 export * from './command.base';
 export * from './domain-event.base';
 export * from './mapper.interface';
-export * from './repository.port';
+export * from './aggregate-root.base';
---
+++ src/libs/types/index.ts
@@ -0,0 +1,6 @@
+/** Consider creating a bunch of shared custom utility
+ * types for different situations.
+ * Alternatively you can use a library like
+ * https://github.com/andnp/SimplyTyped
+ */
+export * from './object-literal.type';
